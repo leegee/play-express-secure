3cc22b582c4d69eef36100b8b1d21875e515ff15
Lee Goddard <lee@leegoddard.net>
2017-05-11 20:30:00 +0200

    wip

--- src/Server.js
+++ src/Server.js
@@ -1,9 +1,36 @@
 const express   = require('express'),
   helmet        = require('helmet'),
-  cookieSession = require('cookie-session');  
+  cookieSession = require('cookie-session'),
+  cookieParser  = require('cookie-parser'),
+  bodyParser    = require('body-parser'),
+  csrf          = require('csurf');
 
-var app = express();
-app.use(helmet());  
+const app = express();
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser());
+
+const csrfProtection = csrf({ cookie: true });  
+
+const parseForm = bodyParser.urlencoded({ extended: false });
+
+app.get('/form', csrfProtection, function(req, res) {  
+  // pass the csrfToken to the view 
+  res.render('send', { csrfToken: req.csrfToken() });
+});
+
+app.post('/process', parseForm, csrfProtection, function(req, res) {  
+  res.send('data is being processed');
+});
+
+/*
+<form action="/process" method="POST">  
+  <input type="hidden" name="_csrf" value="{{csrfToken}}">
+
+  Favorite color: <input type="text" name="favoriteColor">
+  <button type="submit">Submit</button>
+</form>
+*/
 
 app.use(cookieSession({  
   name: 'session',
